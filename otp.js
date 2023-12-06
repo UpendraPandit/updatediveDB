0b45d4f161785509f53bc49c385f984faaa69270
UpendraPandit <126135709+UpendraPandit@users.noreply.github.com>
2023-12-06 18:55:04 +0100

    Update otp.js

--- otp.js
+++ otp.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require('mongodb');
-const url = 'mongodb+srv://doadmin:T2673ly9L4VD15BY@db-mongodb-nyc1-89412-a673e063.mongo.ondigitalocean.com/otp?tls=true&authSource=admin'
+const url = 'mongodb+srv://doadmin:m362r974N80LyiXF@RealtimeDb-c9e25d1e.mongo.ondigitalocean.com/otp?tls=true&authSource=admin'
 const client = new MongoClient(url);
 
 async function otp() {
