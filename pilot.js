1b147be6ed8a55171bbded41624886e2eb4ea642
UpendraPandit <126135709+UpendraPandit@users.noreply.github.com>
2024-06-04 12:16:18 +0530

    Update pilot.js

--- pilot.js
+++ pilot.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require('mongodb');
-const url = 'mongodb+srv://doadmin:7s38mU4x09d2tkl5@PilotPassenger-3452c069.mongo.ondigitalocean.com/admin?tls=true&authSource=admin';
+const url = 'mongodb+srv://doadmin:J532R41TN9e7L6qY@live-database-26594717.mongo.ondigitalocean.com/admin?authSource=admin&replicaSet=live-database&tls=true';
 const client = new MongoClient(url);
 
 async function dbConnection() {
